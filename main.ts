35eb2687de7de51b533ae3bc5b49054794528acf
devon-romesh <devon.romesh@stu.ocsb.ca>
2026-03-13 13:02:59 -0400

    code and comments

--- main.ts
+++ main.ts
@@ -1,8 +1,33 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Devon
+ * Created on: Mar 2026
+ * This program lights up neopixels determind by light.
 */
 
-basic.showString('Hello, World!')
+//variables
+let lightLevels: number = 0
+const myNeopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+//setup
+basic.showIcon(IconNames.Happy)
+myNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+myNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+myNeopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+myNeopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+
+// code for lighting up neopixels
+input.onButtonPressed(Button.A, function() {
+   if (lightLevels > 52){
+   myNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    }
+    if (lightLevels > 104) {
+        myNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    }
+    if (lightLevels > 156) {
+        myNeopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    } 
+    if (lightLevels > 208) {
+        myNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    }
+})
