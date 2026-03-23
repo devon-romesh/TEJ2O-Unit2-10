95e8f5ab953faf40fc50c5a7a9b7cf4357742c42
devon-romesh <devon.romesh@stu.ocsb.ca>
2026-03-23 12:39:29 -0400

    updated and corrected code

--- main.ts
+++ main.ts
@@ -11,23 +11,31 @@ const myNeopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 
 //setup
 basic.showIcon(IconNames.Happy)
-myNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-myNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-myNeopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-myNeopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+myNeopixelStrip.clear()
+myNeopixelStrip.show()
 
 // code for lighting up neopixels
-input.onButtonPressed(Button.A, function() {
-   if (lightLevels > 52){
-   myNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+input.onButtonPressed(Button.A, function () {
+
+    lightLevels = input.lightLevel()
+
+    myNeopixelStrip.clear()
+
+    if (lightLevels > 52) {
+        myNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
     }
+
     if (lightLevels > 104) {
         myNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
     }
+
     if (lightLevels > 156) {
-        myNeopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
-    } 
-    if (lightLevels > 208) {
-        myNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        myNeopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
     }
+
+    if (lightLevels > 208) {
+        myNeopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    }
+
+    myNeopixelStrip.show()
 })
