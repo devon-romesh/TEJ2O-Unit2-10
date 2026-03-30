73059b6dc6a7f1075a966d54b3470b1d2e2b5815
devon-romesh <devon.romesh@stu.ocsb.ca>
2026-03-30 22:50:49 +0000

    fixed code

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -8,7 +8,7 @@ from microbit import *
 import neopixel 
 
 # the variables needed
-lightLevel = 0
+lightLevels = 0
 myNeopixelStrip = neopixel.NeoPixel(pin16, 4)
 
 # setup
@@ -19,23 +19,23 @@ myNeopixelStrip.show()
 # running Button A
 while True:
     if button_a.is_pressed():
-        lightLevel = display.read_light_level()
+        lightLevels = display.read_light_level()
         myNeopixelStrip.clear()
 
         # if light level is less than 52
-        if lightLevel > 52:
-            myNeopixelStrip[0] = (255, 0, 0) 
+        if lightLevels > 52:
+            myNeopixelStrip[0] = (255, 255, 255) 
 
         # if light level is less than 104
-        if lightLevel > 104:
+        if lightLevels > 104:
             myNeopixelStrip[1] = (255, 255, 255)
 
         # if light level is less than 156
-        if lightLevel > 156:
+        if lightLevels > 156:
             myNeopixelStrip[2] = (255, 255, 255)
 
         # if light level is less than 208
-        if lightLevel > 208:
+        if lightLevels > 208:
             myNeopixelStrip[3] = (255, 255, 255)
         myNeopixelStrip.show()
-        display.scroll("Light level is " + (lightLevel))
+        display.scroll("Light level is " + (lightLevels))
